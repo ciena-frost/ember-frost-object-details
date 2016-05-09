3487a8f85b4f7b880cac98a215c88f07fb075d54
Quincy <qle@ciena.com>
2016-05-08 23:56:39 -0400

    update package.json ember-cli-build and blueprints

---
+++ blueprints/ember-frost-modal-input/index.js
@@ -0,0 +1,18 @@
+module.exports = {
+  description: '',
+  normalizeEntityName: function () {},
+
+  /**
+     Installs specified packages at the root level of the application.
+     Triggered by 'ember install <addon name>'.
+
+     @returns {Promise} package names and versions
+   */
+  afterInstall: function () {
+    return this.addAddonsToProject({
+      packages: [
+        {name: 'ember-frost-core', target: '>=0.8.0 <2.0.0'}
+      ]
+    })
+  }
+}
