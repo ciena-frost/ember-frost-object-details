68f7806054ee6db8874ac847b819aab726e78224
Jeremy Brown <jeremy@notmessenger.com>
2017-11-13 16:25:05 -0600

    Back to where we started; weird stuff happening

--- index.js
+++ index.js
@@ -3,9 +3,5 @@
 'use strict'
 
 module.exports = {
-  name: 'ember-frost-object-details',
-
-  included: function (app) {
-    this._super.included.apply(this, app)
-  }
+  name: 'ember-frost-object-details'
 }
